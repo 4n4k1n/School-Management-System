6e22aeaa60eee6ca209c2f2fea96df653d05a347
4n4k1n <anakin@pregitzer.de>
2024-12-29 23:23:45 +0100

    students funktions

--- students.c
+++ students.c
@@ -29,15 +29,17 @@ void print_student(const Student *student)
 {
     if (!student)
         return;
-    printf("ID: %d\n", student->student_id);
-    printf("NAME: %s\n", student->name);
-    printf("AGE: %d\n", student->age);
-    printf("COURSES: %d\n\n", student->count_courses);
+    printf("ID      : %d\n", student->student_id);
+    printf("NAME    : %s\n", student->name);
+    printf("AGE     : %d\n", student->age);
+    printf("COURSES : %d\n\n", student->count_courses);
 }
 
 void get_student_data(Student *student, int size)
 {
     student[size - 1].student_id = size;
+    student[size - 1].count_courses = 0;
+    student[size - 1].grades = NULL;
     printf("Enter student name: ");
     scanf(" %49[^\n]", student[size - 1].name);
     printf("Enter student age: ");
@@ -49,17 +51,17 @@ void f_put_student_data(Student *student, int size)
     FILE *file;
 
     file = fopen("students.txt", "a");
-    fprintf(file, "%d,%s,%d\n", (student[size - 1]).student_id, (student[size - 1]).name, (student[size - 1]).age);
+    fprintf(file, "%d,%s,%d,%d\n", (student[size - 1]).student_id, (student[size - 1]).name, (student[size - 1]).age, student->count_courses);
     fclose(file);
 }
 
-Student *add_student(Student *students, int *size)
+Student *add_student(Structs *structs)
 {
-    (*size)++;
-    Student *new_students = (Student *)realloc(students, sizeof(Student) * (*size));
+    structs->lengths[0]++;
+    Student *new_students = (Student *)realloc(structs->students, sizeof(Student) * structs->lengths[0]);
     if (new_students == NULL)
-        return students;
-    get_student_data(new_students, *size);
-    f_put_student_data(new_students, (*size));
-    return new_students;
+        return (structs->students);
+    get_student_data(new_students, structs->lengths[0]);
+    f_put_student_data(new_students, structs->lengths[0]);
+    return (new_students);
 }
--- students.h
+++ students.h
@@ -9,6 +9,6 @@ void load_student_data(FILE *file, Student *students, int size);
 void print_student(const Student *student);
 void get_student_data(Student *student, int size);
 void f_put_student_data(Student *student, int size);
-Student *add_student(Student *students, int *size);
+Student *add_student(Structs *structs);
 
 #endif
